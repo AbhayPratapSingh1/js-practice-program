d963fce118e15e2bb3a9be7ceb961bd14b48a5b3
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 19:06:28 +0530

    Create palindrome_approach_2.js, added basic flow of the program

---
+++ pallindrome/palindrome_approach_2.js
@@ -0,0 +1,20 @@
+const sampleCase1 = "a"
+const sampleCase2 = "ab"
+const sampleCase3 = "abcb"
+const sampleCase4 = "abcba"
+const sampleCase5 = " "
+const sampleCase6 = " a a "
+const sampleCase7 = "racecar"
+const sampleCase8 = ""
+
+const sampleCase = sampleCase1
+
+let isPalindrome = true
+const checkIsSame = true
+
+isPalindrome = checkIsSame? isPalindrome : false
+
+
+
+const result = isPalindrome ?"is pallindrome": "is not a pallindrome"
+console.log(sampleCase, result)
