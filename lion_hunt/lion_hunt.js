61f24f71ba6f8f3de648b3da4fe9fb7a7f99aa4b
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 16:10:08 +0530

    updated lion_hunt.js, added somecase, added to run till case 2c

--- lion_hunt/lion_hunt.js
+++ lion_hunt/lion_hunt.js
@@ -2,14 +2,15 @@ const sample1 = "LZ"        // 0
 const sample1b = " LZ"        // 0
 const sample2 = "Z L"       // 1
 const sample2b = " Z L"       // 1
+const sample2c = "  Z L"       // 1
 const sample3 = "L     Z"   // 5
 const sample4 = "L     L"   // -1
 const sample5 = "Z   Z   Z" // -1
 const sample6 = "L  ZL Z"   // 0
-const sample7 = ""          // 0
-const sample8 = "LZ  L Z"   // 2
+const sample7 = ""          // -1
+const sample8 = "LZ  L Z"   // 0
 
-const sampleCase = sample1b;
+const sampleCase = sample2c;
 let spaceBetween = -1;
 
 let lastAnimal = "";
@@ -17,16 +18,11 @@ let secondLastAnimal = "";
 
 let sampleIteratorIndex = 0;
 
-
-if (sampleIteratorIndex < sampleCase.length && sampleCase[sampleIteratorIndex] !== " "){
-  secondLastAnimal = lastAnimal
-  lastAnimal = sampleCase[sampleIteratorIndex];
-}
-sampleIteratorIndex += 1
-
 for (; sampleIteratorIndex < sampleCase.length; sampleIteratorIndex++){
-  if (sampleCase[sampleIteratorIndex]===" "){
-    spaceBetween = spaceBetween + 1
+  if (sampleCase[sampleIteratorIndex] === " " ){
+    if (lastAnimal !== ""){
+      spaceBetween = spaceBetween + 1
+    }
   }
   else{
     secondLastAnimal = lastAnimal
@@ -36,8 +32,6 @@ for (; sampleIteratorIndex < sampleCase.length; sampleIteratorIndex++){
     }
   }
 }
-
-
 
 const output = spaceBetween;
 console.log(output);
