8e4d2c96c4ec51fa73ce2117a31f1a9630ccf4ec
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 19:15:35 +0530

    Update palindrome_approach_2.js, checking first and last character in the string

--- pallindrome/palindrome_approach_2.js
+++ pallindrome/palindrome_approach_2.js
@@ -1,5 +1,5 @@
 const sampleCase1 = "a"
-const sampleCase2 = "ab"
+const sampleCase2 = "abca"
 const sampleCase3 = "abcb"
 const sampleCase4 = "abcba"
 const sampleCase5 = " "
@@ -10,10 +10,16 @@ const sampleCase8 = ""
 const sampleCase = sampleCase1
 
 let isPalindrome = true
-const checkIsSame = true
+let checkIsSame = true
 
-isPalindrome = checkIsSame? isPalindrome : false
 
+let sampleIteratingIndex = 0;
+
+if (isPalindrome && sampleIteratingIndex < sampleCase.length){
+  const sampleNegativeIndex = sampleCase.length - sampleIteratingIndex - 1
+  checkIsSame = sampleCase[sampleIteratingIndex] === sampleCase[sampleNegativeIndex]
+  isPalindrome = checkIsSame? isPalindrome : false
+}
 
 
 const result = isPalindrome ?"is pallindrome": "is not a pallindrome"
