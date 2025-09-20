d6e910c68860a72421e372a90d8966686b33b154
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 19:26:05 +0530

    Update palindrome_approach_2.js, reduce the no of iteration to half
    we are matching string items twice once first half to other half and then again second half to the first half

--- pallindrome/palindrome_approach_2.js
+++ pallindrome/palindrome_approach_2.js
@@ -7,7 +7,7 @@ const sampleCase6 = " a a "
 const sampleCase7 = "racecar"
 const sampleCase8 = ""
 
-const sampleCase = sampleCase8
+const sampleCase = sampleCase5
 const sampleLength = sampleCase.length
 
 let isPalindrome = true
@@ -16,7 +16,7 @@ let sampleIteratingIndex = 0;
 
 
 
-for( let sampleIteratingIndex = 0; isPalindrome && (sampleIteratingIndex < sampleLength); sampleIteratingIndex++){
+for( let sampleIteratingIndex = 0; isPalindrome && (sampleIteratingIndex < sampleLength/2); sampleIteratingIndex++){
   const sampleNegativeIndex = sampleLength - sampleIteratingIndex - 1
   checkIsSame = sampleCase[sampleIteratingIndex] === sampleCase[sampleNegativeIndex]
   isPalindrome = checkIsSame? isPalindrome : false
