ac861980735abfec13ee55353569443b29f38fed
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 15:27:28 +0530

    Update lion_hunt.js for sample cases 1, 1b, 2, 2b

--- lion_hunt/lion_hunt.js
+++ lion_hunt/lion_hunt.js
@@ -1,6 +1,7 @@
 const sample1 = "LZ"        // 0
 const sample1b = " LZ"        // 0
 const sample2 = "Z L"       // 1
+const sample2b = " Z L"       // 1
 const sample3 = "L     Z"   // 5
 const sample4 = "L     L"   // -1
 const sample5 = "Z   Z   Z" // -1
@@ -8,7 +9,7 @@ const sample6 = "L  ZL Z"   // 0
 const sample7 = ""          // 0
 const sample8 = "LZ  L Z"   // 2
 
-const sampleCase = sample1b;
+const sampleCase = sample2;
 let spaceBetween = -1;
 
 let lastAnimal = "";
@@ -24,22 +25,72 @@ if (sampleIteratorIndex < sampleCase.length && sampleCase[sampleIteratorIndex] !
 sampleIteratorIndex += 1
 
 
-if (sampleIteratorIndex < sampleCase.length && sampleCase[sampleIteratorIndex] !== " "){
-  secondLastAnimal = lastAnimal
-  lastAnimal = sampleCase[sampleIteratorIndex];
-  if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
-    spaceBetween = spaceBetween + 1;
+if (sampleIteratorIndex < sampleCase.length){
+  if (sampleCase[sampleIteratorIndex]===" "){
+    spaceBetween = spaceBetween + 1
+  }
+  else{
+    secondLastAnimal = lastAnimal
+    lastAnimal = sampleCase[sampleIteratorIndex];
+    if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
+      spaceBetween = spaceBetween + 1;
+    }
   }
 }
 sampleIteratorIndex += 1
 
+if (sampleIteratorIndex < sampleCase.length){
+  if (sampleCase[sampleIteratorIndex]===" "){
+    spaceBetween = spaceBetween + 1
+  }
+  else{
+    secondLastAnimal = lastAnimal
+    lastAnimal = sampleCase[sampleIteratorIndex];
+    if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
+      spaceBetween = spaceBetween + 1;
+    }
+  }
+}
+sampleIteratorIndex += 1
 
+if (sampleIteratorIndex < sampleCase.length){
+  if (sampleCase[sampleIteratorIndex]===" "){
+    spaceBetween = spaceBetween + 1
+  }
+  else{
+    secondLastAnimal = lastAnimal
+    lastAnimal = sampleCase[sampleIteratorIndex];
+    if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
+      spaceBetween = spaceBetween + 1;
+    }
+  }
+}
+sampleIteratorIndex += 1
 
-if (sampleIteratorIndex < sampleCase.length && sampleCase[sampleIteratorIndex] !== " "){
-  secondLastAnimal = lastAnimal
-  lastAnimal = sampleCase[sampleIteratorIndex];
-  if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
-    spaceBetween = spaceBetween + 1;
+if (sampleIteratorIndex < sampleCase.length){
+  if (sampleCase[sampleIteratorIndex]===" "){
+    spaceBetween = spaceBetween + 1
+  }
+  else{
+    secondLastAnimal = lastAnimal
+    lastAnimal = sampleCase[sampleIteratorIndex];
+    if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
+      spaceBetween = spaceBetween + 1;
+    }
+  }
+}
+sampleIteratorIndex += 1
+
+if (sampleIteratorIndex < sampleCase.length){
+  if (sampleCase[sampleIteratorIndex]===" "){
+    spaceBetween = spaceBetween + 1
+  }
+  else{
+    secondLastAnimal = lastAnimal
+    lastAnimal = sampleCase[sampleIteratorIndex];
+    if ( secondLastAnimal !== "" && lastAnimal !== secondLastAnimal){
+      spaceBetween = spaceBetween + 1;
+    }
   }
 }
 sampleIteratorIndex += 1
