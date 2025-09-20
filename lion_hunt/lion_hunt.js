6c88bcc8556522343ab44fd6d9660c5b097c0fea
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-20 14:30:49 +0530

    added sample cases for the lion_hunt problem

--- lion_hunt/lion_hunt.js
+++ lion_hunt/lion_hunt.js
@@ -1,1 +1,13 @@
-console.log("Is it working? Test 3")
+console.log("Is it working? ..")
+
+
+const sample1 = "LZ"        // 0
+const sample2 = "Z L"       // 1
+const sample3 = "L     Z"   // 5
+const sample4 = "L     L"   // -1
+const sample5 = "Z   Z   Z" // -1
+const sample6 = "L  ZL Z"   // 0
+const sample7 = ""          // 0
+const sample8 = "LZ  L Z"   // 2
+
+const checkingSample = sample1;
