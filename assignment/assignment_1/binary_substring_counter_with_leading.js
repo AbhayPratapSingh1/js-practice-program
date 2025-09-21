8d4a79a9bdc4e87a00b40fb5543a319d5e2bc4f3
Abhay Pratap Singh <162894796+AbhayPratapSingh1@users.noreply.github.com>
2025-09-21 11:53:22 +0530

    Create binary_substring_counter_with_leading.js

---
+++ assignment/assignment_1/binary_substring_counter_with_leading.js
@@ -0,0 +1,30 @@
+const sample1_mainNo = 65
+const sample1_substring = "10"
+// 1
+
+const sample2_mainNo = 65
+const sample2_substring = "00"
+// 4
+
+const sample3_mainNo = 21
+const sample3_substring = "101"
+// 2
+
+const sample4_mainNo = 31
+const sample4_substring = "11"
+//4
+
+const sample5_mainNo = 8
+const sample5_substring = "01"
+// 1
+
+const sample6_mainNo = 19
+const sample6_substring = "1"
+// 3
+
+
+const mainNo = sample1_mainNo
+const substring = sample1_substring
+
+
+console.log("Testing 2")
