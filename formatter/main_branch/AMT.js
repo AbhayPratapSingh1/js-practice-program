0be19fb651b6ebb9277267d2944b296a6a397f58
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2025-11-11 23:28:36 +0530

    added the comment cutting function

--- formatter/main_branch/AMT.js
+++ formatter/main_branch/AMT.js
@@ -1,25 +1,48 @@
 import { dbg } from "./__debugger-helper.js";
-import { KNOWN_CHARS, SPACES } from "./__staticData.js";
+import { COMMENTS_PAIR, KNOWN_CHARS, SPACES } from "./__staticData.js";
 
 export const isKnownCharacter = (element) => KNOWN_CHARS.includes(element);
+
+export const isComment = (element) => {
+  return Object.keys(COMMENTS_PAIR).includes(element);
+};
 
 export const formatCharacter = (element) => {
   const index = SPACES.findIndex((each) => each.values.includes(element));
   return index === -1 ? element : SPACES[index].format(element);
 };
 
-// THIS CONTAIN THE STRING STARTING CHARACTER TO!
+const cutLineStart = (line, finalSize) => {
+  return line.slice(line.length - finalSize, line.length);
+};
+
+// should i merge both of them?
 export const cutString = (line, curIndex) => {
   const stringStartingIcon = line[curIndex];
   let itrIndex = curIndex + 1;
+
   while (itrIndex < line.length && line[itrIndex] !== stringStartingIcon) {
     itrIndex++;
   }
 
-  return {
-    index: itrIndex + 1,
-    cuttedString: line.slice(curIndex, itrIndex + 1),
-  };
+  const strLine = line.slice(curIndex, itrIndex + 1);
+  return { index: itrIndex, cuttedString: strLine };
+};
+
+export const cutComment = (line, curIndex) => {
+  const endChar = COMMENTS_PAIR[line.slice(curIndex, curIndex + 2)];
+  const matSize = endChar.length;
+
+  let itr = curIndex + 2;
+  let checkingChar = "";
+
+  while (itr < line.length && checkingChar !== endChar) {
+    itr++;
+    checkingChar = cutLineStart(line.slice(itr - 1, itr + 1), matSize);
+  }
+
+  const strLine = line.slice(curIndex, itr + 1);
+  return { index: itr, cuttedString: strLine };
 };
 
 export const AMT = (code) => {
