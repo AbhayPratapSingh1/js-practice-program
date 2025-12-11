b2a05dbbd8c1ba4f1d9e2c8f8dcbb77cc7237cce
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2025-12-11 21:47:27 +0530

    created similar working like iterator

---
+++ iterator/src/generator.js
@@ -0,0 +1,58 @@
+const generator = () => {
+  let i = 0;
+
+  return {
+    take(n) {
+      let c = 0;
+      const prevNext = this.next;
+
+      const newNext = (...args) => {
+        return c++ < n ? prevNext(...args) : { done: true };
+      };
+
+      this.next = newNext;
+      return this;
+    },
+
+    map(fn) {
+      const prevNext = this.next;
+
+      const newNext = (...args) => {
+        const newVal = prevNext(...args);
+        if (!newVal.done) {
+          return { value: fn(newVal.value), done: false };
+        }
+        return newVal;
+      };
+
+      this.next = newNext;
+      return this;
+    },
+
+    filter(fn) {
+      const prevNext = this.next;
+
+      const newNext = (...args) => {
+        const newVal = prevNext(...args);
+        if (!newVal.done) {
+          return fn(newVal.value) ? newVal : newNext(...args);
+        }
+
+        return newVal;
+      };
+
+      this.next = newNext;
+      return this;
+    },
+    next() {
+      return { done: false, value: i++ };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
+};
+
+const itr = generator();
+
+[...itr.filter((x) => x % 2 === 0).take(5)];
