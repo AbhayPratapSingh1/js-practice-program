837337b3c1f61afe8bc88feab57872e71f4b8450
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2025-12-14 15:53:50 +0530

    added basic debugger with the animation one too

---
+++ intcode/main.js
@@ -0,0 +1,9 @@
+import { runDebugger } from "./debugger/debugger.js";
+import { makeComputer, runComputer } from "./src/intcode.js";
+const program = Deno.readTextFileSync(
+  "./test/data/additionMultiplicationBoss.txt",
+).split(",").map((each) => +each);
+const computer = makeComputer(program, [[1, 12], [2, 2]]);
+
+runDebugger(computer, true);
+console.log(computer.program[0]);
