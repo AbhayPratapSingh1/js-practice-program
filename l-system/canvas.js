13be3752a6830d66e6f4baca576cf546a63057b7
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-01-01 21:40:29 +0530

    create basic turtle

--- l-system/canvas.js
+++ l-system/canvas.js
@@ -1,51 +1,58 @@
 import { createScreen, displayScreen } from "./drawScreen.js";
-import { arc, circle, pieceOfCake, polygon } from "./shapes.js";
+import { line, toRadian } from "./shapes.js";
 
 const configData = await Deno.readTextFile("./canvasConfig.json");
 const config = JSON.parse(configData);
 
 const screen = createScreen(config);
 
-const center = { x: config.width / 2, y: config.height / 2 };
+class Turtle {
+  constructor(x, y, screen, angle = 270) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.screen = screen;
+  }
 
-const createEye = (screen, x, dy = 0, radius = 120) => {
-  const diff = Math.sqrt(3) * radius / 2;
+  // XX XX pp XX XX
+  // XX XX XX XX XX
+  // XX XX oo XX XX
+  // XX XX XX XX XX
+  // XX XX XX XX XX
 
-  const c1 = { x, y: dy + (config.height / 2) - diff };
-  const c2 = { x, y: dy + (config.height / 2) + diff };
+  move(steps) {
+    const angle = toRadian(this.angle);
+    const dx = Math.cos(angle) * steps;
+    const dy = Math.sin(angle) * steps;
 
-  const pC1 = { y: dy + (config.height / 2), x };
+    const prev = { x: this.x, y: this.y };
 
-  const pupilRadius = radius - diff - 1;
-  circle(pC1, pupilRadius, screen);
-  arc(c1, 60, 120, radius, screen, false);
-  arc(c2, 240, 300, radius, screen, false);
-};
+    this.x += dx;
+    this.y += dy;
 
-const createNose = (screen, center) => {
-  polygon({ x: center.x, y: center.y + 10 }, 30, 3, screen, 30);
-};
+    line(
+      prev,
+      { x: this.x, y: this.y },
+      this.screen,
+    );
+  }
+  rotate(angle, clockWise = true) {
+    if (clockWise) {
+      this.angle += angle;
+      return;
+    }
+    this.angle -= angle;
+  }
+}
 
-const createMouth = (screen, center) => {
-  const bigCircleCenter = { x: center.x, y: center.y - 10 };
-  const bigCircleRadius = 100;
-  const smallCircleCenter = {
-    x: center.x,
-    y: (bigCircleRadius / 2) + bigCircleCenter.y + 20,
-  };
+const turtle = new Turtle(screen.width / 2, screen.height / 2, screen);
 
-  arc(bigCircleCenter, 60, 120, bigCircleRadius, screen, false);
-  arc(smallCircleCenter, 70, 110, bigCircleRadius / 2, screen, false);
-};
-
-const createFace = (screen, center) => {
-  createEye(screen, screen.width / 4, -50);
-  createEye(screen, 3 * (screen.width / 4), -50);
-
-  createNose(screen, center);
-  createMouth(screen, center);
-};
-
-createFace(screen, center);
+turtle.move(20);
+turtle.rotate(90, true);
+turtle.move(20);
+turtle.rotate(90, false);
+turtle.move(20);
+turtle.rotate(45, false);
+turtle.move(20);
 
 displayScreen(screen);
